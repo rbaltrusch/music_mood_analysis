9a816de9a1d2105f95bbc694ad3ce89e12634b1b
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-01-12 22:42:58 +0100

    Add beat distance hypothesis allowance percentage to consts

--- music_mood_analysis/consts.py
+++ music_mood_analysis/consts.py
@@ -8,7 +8,7 @@ Created on Tue Jan 12 14:47:09 2021
 SAMPLE_RATE = 44100
 
 CONVERSION_RATIO = 32
-CHUNK_SIZE = 100
+CHUNK_SIZE = 5
 
 BPS_MAX = 3 #how many beats per second we detect at most
 BPS_MIN = 1.5 #how many beats per second we detect at least
@@ -18,11 +18,13 @@ MUSICAL_NOTE_FREQUENCIES = [440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25,
 MUSICAL_NOTE_LOWER_BOUND = 440
 MUSICAL_NOTE_HIGHER_BOUND = 881
 MUSICAL_NOTE_NAMES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+                      
+BEAT_DISTANCE_HYPOTHESIS_ALLOWANCE_PERCENTAGE = 0.1
 
 PLOTTING_ENABLED = True
 
 def get_beat_distance_constants(samplerate):
     '''returns min and max distances between beats (int, int)'''
-    beat_distance_min = samplerate / BPS_MAX
-    beat_distance_max = samplerate / BPS_MIN
+    beat_distance_min = round(samplerate / BPS_MAX)
+    beat_distance_max = round(samplerate / BPS_MIN)
     return beat_distance_min, beat_distance_max
