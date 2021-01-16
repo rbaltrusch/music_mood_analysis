6cf82eb609859ee344b219b414e3cd45fcba4577
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-01-16 15:36:10 +0100

    Add decorator result checking to test_timeit_returns_function

--- tests/unit/util_test.py
+++ tests/unit/util_test.py
@@ -5,7 +5,15 @@ Created on Fri Jan 15 12:46:38 2021
 @author: Korean_Crimson
 """
 
+import pytest
 from music_mood_analysis import util
 
-def test_timeit_returns_function():
-    assert callable(util.timeit(lambda: None)), 'timeit decorator must return a function'
+@pytest.mark.parametrize("function,expected", [(lambda: None, None),
+                                      (lambda: 1, 1),
+                                      (lambda: [1], [1])
+                                      ])
+def test_timeit_returns_function(function, expected):
+    func = util.timeit(function)
+    assert callable(func), 'timeit decorator must return a function'
+    result = func()
+    assert result == expected, 'timeit decorator should not change result of decorated function'
