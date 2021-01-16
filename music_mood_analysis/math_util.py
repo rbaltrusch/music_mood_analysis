40542b9927d30ff87e67353241ce1dbb47f6876a
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-01-16 14:03:18 +0100

    Add docstring to math_util.get_index_of

--- music_mood_analysis/math_util.py
+++ music_mood_analysis/math_util.py
@@ -22,11 +22,25 @@ def compute_Yss(samplerate, data):
         Yss: numpy.array (one-dimensional discrete Fourier Transform)
         Yss_f: list (Discrete Fourier Transform sample non-zero positive frequencies)
     '''
+    #pylint: disable=C0103
     Yss = numpy.fft.fft(data)
     time_step = 1 / samplerate
     Yss_f = [freq for freq in numpy.fft.fftfreq(Yss.size, time_step) if freq > 0]
     return Yss, Yss_f
 
 def get_index_of(func, data):
+    """returns index of the data point which func (should be in-built min or max)
+    would return if called by itself.
+
+    Input args:
+        func: function (should be min/max)
+        data: iterable
+
+    Return val:
+        index: int
+
+    Example:
+        index = get_index_of(max, [3, 5, 2]) #returns 1
+    """
     index, _ = func(enumerate(data), key=lambda x: x[1])
     return index
