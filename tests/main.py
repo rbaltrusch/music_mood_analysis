8c7f0c58dbc468cfbbe62a2ce32bda246eec45a2
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-01-16 12:19:30 +0100

    Add tests/main.py to run pytest

--- tests/main.py
+++ tests/main.py
@@ -5,8 +5,18 @@ Created on Thu Jan 14 23:39:42 2021
 @author: Korean_Crimson
 """
 
-import py
 import sys
+import subprocess
+import py
 
-sys.path.append('..\\music_mood_analysis')
-py.test.cmdline.main() #['--junitxml']
+def main():
+    """Add package under test to PYTHONPATH, run pytest to generate html report
+    and open the report in the browser.
+    """
+    sys.path.append('..\\music_mood_analysis')
+    #pylint: disable=E1101
+    py.test.cmdline.main(args=['--html=report.html', '--self-contained-html'])
+    subprocess.call('start report.html', shell=True)
+
+if __name__ == '__main__':
+    main()
