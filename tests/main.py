5c6da66667fa383886d6fb20df27adb116d77907
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-01-16 13:31:29 +0100

    Add open_in_browser kwarg to tests/main.py

--- tests/main.py
+++ tests/main.py
@@ -9,14 +9,16 @@ import sys
 import subprocess
 import py
 
-def main():
+def main(open_in_browser=False):
     """Add package under test to PYTHONPATH, run pytest to generate html report
     and open the report in the browser.
     """
     sys.path.append('..\\music_mood_analysis')
     #pylint: disable=E1101
     py.test.cmdline.main(args=['--html=report.html', '--self-contained-html'])
-    subprocess.call('start report.html', shell=True)
+
+    if open_in_browser:
+        subprocess.call('start report.html', shell=True)
 
 if __name__ == '__main__':
     main()
