379aaba14ddc69bd2cd598ff357fe61e7ed9d2f0
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-01-16 15:00:01 +0100

    Add coverage to tests/main.py, factor out PACKAGE_NAME

--- tests/main.py
+++ tests/main.py
@@ -5,17 +5,25 @@ Created on Thu Jan 14 23:39:42 2021
 @author: Korean_Crimson
 """
 
+import os
 import sys
 import subprocess
 import py
+
+PACKAGE_NAME = 'music_mood_analysis'
 
 def main(open_in_browser=False):
     """Add package under test to PYTHONPATH, run pytest to generate html report
     and open the report in the browser.
     """
-    sys.path.append('..\\music_mood_analysis')
+    package_path = os.path.join('..', PACKAGE_NAME)
+    sys.path.append(package_path)
+
     #pylint: disable=E1101
-    py.test.cmdline.main(args=['--html=report.html', '--self-contained-html'])
+    py.test.cmdline.main(args=['--html=report.html',
+                               '--self-contained-html',
+                               f'--cov={package_path}'
+                               ])
 
     if open_in_browser:
         subprocess.call('start report.html', shell=True)
